fc213cf674d3de51f3d4419f90f69a4b75eaa66d
“ameybhosle212” <ameybhosle212@gmail.com>
2023-04-28 15:27:24 +0530

    Fix: inde.js and added versel.json

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,16 @@ app.use(express.urlencoded({extended:true}))
 app.use('/',require("./routes/index"))
 
 
+if(process.env.NODE_ENV=='production'){
+    const path = require('path')
+
+    app.get('/',(req,res)=>{
+        app.use(express.static(path.resolve(path.join(__dirname,'../'),'frontend','build')))
+        res.sendFile(path.resolve(path.join(__dirname,'../'),'frontend','build','index.html'))
+    })
+}
+
+
 // Server 
 app.listen(2200,()=>{
     console.log("Server at 2200");
